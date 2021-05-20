fcc9f57b3c70edb5c841d97e92136929ca139305
EvgenPrushk <ogmprushak@mail.ru>
2021-05-20 23:13:56 +0300

    feat: add route

--- mern-cource/client/src/pages/AuthPage.js
+++ mern-cource/client/src/pages/AuthPage.js
@@ -1,8 +1,10 @@
 import React from "react";
 export const AuthPage = () => {
   return (
-    <div>
-      <h1>Auth Page</h1>
+    <div className="row">
+      <div className="col s6 offset-s3">
+        <h1>Сократи ссылку</h1>
+      </div>
     </div>
   );
 };
