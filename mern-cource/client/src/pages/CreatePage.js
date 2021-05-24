54d71f27d83a76d1c42bb745b65caf21348b11e4
EvgenPrushk <ogmprushak@mail.ru>
2021-05-24 20:42:09 +0300

    feat: add DetailPage

--- mern-cource/client/src/pages/CreatePage.js
+++ mern-cource/client/src/pages/CreatePage.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
-import { request } from "express";
+import React, { useState, useEffect, useContext } from "react";
+import { useHttp } from "../hooks/http.hook";
+import { AuthContext } from "../context/AuthContext";
+import { useHistory } from "react-router-dom";
 
 export const CreatePage = () => {
-  const {} = useHttp();
+  const history = useHistory();
+  const auth = useContext(AuthContext);
+  const { request } = useHttp();
   const [link, setLink] = useState("");
+
+  useEffect(() => {
+    window.M.updateTextFields();
+  }, []);
+
   const pressHandler = async (event) => {
     if (event.key === "Enter") {
       try {
-        data = await request("api/link/generate", "POST", { from: link });
+        const data = await request(
+          "api/link/generate",
+          "POST",
+          { from: link },
+          { Authorisation: `Bearer ${auth.token}` }
+        );
+        history.push(`/detail/${data.link._id}`);
       } catch (e) {}
     }
   };
--- mern-cource/client/src/pages/DetailPage.js
+++ mern-cource/client/src/pages/DetailPage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState, useCallback, useContext, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Loader } from "../components/Loader";
+import { AuthContext } from "../context/AuthContext";
+import { useHttp } from "../hooks/http.hook";
+
 export const DetailPage = () => {
-  return (
-    <div>
-      <h1>Links Page</h1>
-    </div>
-  );
+  const { token } = useContext(AuthContext);
+  const { request, loading } = useHttp();
+  const [link, setLink] = useState(null);
+  const linkId = useParams().id;
+  const getLink = useCallback(async () => {
+    try {
+      const fetched = await request(`/api/link/${linkId}`, "GET", null, {
+        Authorisation: `Bearer ${token}`,
+      });
+      setLink(fetched);
+    } catch (e) {}
+  }, [token, linkId, request]);
+
+  useEffect(() => {
+    getLink();
+  }, [getLink]);
+  if (loading) {
+    return <Loader />;
+  }
+  return <>{!loading && link && <LinkCard />}</>;
 };
