aee6150b816c9c27a7a9b059bcf702eb2fc73842
EvgenPrushk <ogmprushak@mail.ru>
2021-05-24 17:21:16 +0300

    feat:  add CreatePage

--- mern-cource/client/src/pages/CreatePage.js
+++ mern-cource/client/src/pages/CreatePage.js
@@ -1,8 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
+import { request } from "express";
+
 export const CreatePage = () => {
+  const {} = useHttp();
+  const [link, setLink] = useState("");
+  const pressHandler = async (event) => {
+    if (event.key === "Enter") {
+      try {
+        data = await request("api/link/generate", "POST", { from: link });
+      } catch (e) {}
+    }
+  };
   return (
-    <div>
-      <h1>Create Page</h1>
+    <div className="row">
+      <input
+        placeholder="Вставте ссылку"
+        id="link"
+        type="text"
+        value={link}
+        className="yellow-input"
+        onChange={(e) => setLink(e.target.value)}
+        onKeyPress={pressHandler}
+      />
+      <label htmlFor="link">Введите ссылку</label>
     </div>
   );
 };
--- mern-cource/routes/link.routes.js
+++ mern-cource/routes/link.routes.js
@@ -1,29 +1,50 @@
 const { Router } = require("express");
-const { check, validationResult } = require("express-validator");
 const config = require("config");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+const shortid = require("shortid");
 const Link = require("../models/Link");
+const auth = require("../middleware/auth.middleware");
 const router = Router();
 
 // //api/auth/register
-router.post("/generate", async (req, res) => {
+router.post("/generate", auth, async (req, res) => {
   try {
+    const baseUrl = config.get("baseUrl");
+    const { from } = req.body;
+
+    const code = shortid.generate();
+
+    const existing = await Link.findOne({ from });
+
+    if (existing) {
+      return res.json({ link: existing });
+    }
+
+    const to = baseUrl + "/t/" + code;
+
+    const link = new link({
+      code,
+      to,
+      from,
+      owner: req.user.userId,
+    });
+
+    await link.save();
+    res.status(201).json({ link });
   } catch (e) {
     res.status(500).json({ message: "Что-то пошло не так, попробуй снова " });
   }
 });
 
-router.get("/", async (req, res) => {
+router.get("/", auth, async (req, res) => {
   try {
-    const links = await Link.find({ owner: null }); //&&
+    const links = await Link.find({ owner: req.user.userId }); //&&
     res.json(links);
   } catch (e) {
     res.status(500).json({ message: "Что-то пошло не так, попробуй снова " });
   }
 });
 
-router.get("/:id", async (req, res) => {
+router.get("/:id", auth, async (req, res) => {
   try {
     const links = await Link.findById(req.params.id); //&&
     res.json(links);
