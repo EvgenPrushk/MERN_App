a90aa737dc7c33b0399987bc3a34dd82a9713ad7
EvgenPrushk <ogmprushak@mail.ru>
2021-05-25 10:52:45 +0300

    refactor: Navbar

--- mern-cource/client/src/components/Navbar.js
+++ mern-cource/client/src/components/Navbar.js
@@ -11,22 +11,24 @@ export const Navbar = () => {
     auth.logout();
     history.push("/");
   };
-  <nav>
-    <div className="nav-wrapper blue darken-1" style={{ padding: "0 2rem" }}>
-      <span className="brand-logo">Сокращение ссылок</span>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li>
-          <NavLink to="/create">Создать</NavLink>
-        </li>
-        <li>
-          <NavLink to="/links">Ссылки</NavLink>
-        </li>
-        <li>
-          <a href="/" onClick={logoutHandler}>
-            Выйти
-          </a>
-        </li>
-      </ul>
-    </div>
-  </nav>;
+  return (
+    <nav>
+      <div className="nav-wrapper blue darken-1" style={{ padding: "0 2rem" }}>
+        <span className="brand-logo">Сокращение ссылок</span>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li>
+            <NavLink to="/create">Создать</NavLink>
+          </li>
+          <li>
+            <NavLink to="/links">Ссылки</NavLink>
+          </li>
+          <li>
+            <a href="/" onClick={logoutHandler}>
+              Выйти
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
 };
